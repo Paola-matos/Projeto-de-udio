c5b4cdadd9cb68e9d2944526b3fd0f6d247cbcaf
Paola-matos <162580325+Paola-matos@users.noreply.github.com>
2025-09-15 12:18:53 -0300

    Add files via upload

---
+++ Projeto-de-udio-main/Javascript.js
@@ -0,0 +1,20 @@
+function tocarSom(id) {
+
+    const audios = document.querySelectorAll('audio');
+    
+    audios.forEach(audio => {
+    
+    audio.pause();
+    
+    audio.currentTime = 0;
+    
+    });
+    
+    const audioSelecionado = document.getElementById(id);
+    
+    if (audioSelecionado) {
+
+    audioSelecionado.play();
+    
+    }
+}
